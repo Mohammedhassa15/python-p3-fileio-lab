25c5a836e512ba10342d424f700863484d394a08
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-08-12 18:00:44 +0300

    file input/output

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    employee_file=open(file_name.with_suffix(".txt"), "w")
+    employee_file.write(file_content)
+
+    employee_file.close()
 
 def append_file(file_name, append_content):
-    pass
+    employee_file=open(file_name.with_suffix(".txt"), "a")
+    employee_file.write(append_content)
+
+    employee_file.close()
 
 def read_file(file_name):
-    pass
+    employee_file=open(file_name.with_suffix(".txt"), "r")
+    content=employee_file.read()
+    return content 
+
+
+    employee_file.close()
